116c1ebdee50b28bd931b0c887d3d259d1efb0f1
Billy <billyjcatchpole@gmail.com>
2020-01-11 09:20:22 +0000

    Removed public and views because this is an API Connected to MongoDb database

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -11,12 +11,6 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/signup", authController.getSignUp);
-router.post("/signup", authController.postSignUp);
-
-router.get("/login", authController.getLogin);
-router.post("/login", authController.postLogin);
-
-router.get("/logout", authController.getLogout);
+router.get("/signup", authController.Signup);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -10,25 +10,12 @@ const authRouter = require("./routes/authRouter");
 const bodyParser = require("body-parser");
 const express = require("express");
 const mongoose = require('mongoose');
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 
 //-File configuration
 
-const MONGODBURI = "mongodb+srv://Billy:bjc123@billy-qqthk.mongodb.net/shopp";
+const MONGODBURI = "mongodb://Billy:bjc123@billy-shard-00-00-qqthk.mongodb.net:27017,billy-shard-00-01-qqthk.mongodb.net:27017,billy-shard-00-02-qqthk.mongodb.net:27017/recall?ssl=true&replicaSet=Billy-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 const server = express();
-
-const store = new MongoDBStore({
-
-    uri: MONGODBURI, //the database to connect to
-    collection: "sessions" // The name of the collection to store them
-  
-  });
-
-server.use(session({secret: "my secret", resave: false, saveUninitialized: false, store: store}));
-
-server.set("view engine", "ejs");//set the templating engine to ejs
 
 server.use(express.static(path.join(__dirname, 'public')));//Allow the html to connect to css pages
 
@@ -47,7 +34,7 @@ mongoose
   )
   .then(result => {
     server.listen(5000);
-    console.log("connection established")
+    console.log("Server running on port 5000")
   })
   .catch(err => {
     console.log(err);
